6896aad84785aacfa4832b256696c21ec8e362a1
palakgu <168259818+palakgu@users.noreply.github.com>
2025-02-12 12:42:46 +0530

    Delete Day01_javaGenerics directory

--- Day01_javaGenerics/src/main/java/personalizedmealplangenerator/VeganMeal.java
+++
@@ -1,8 +0,0 @@
-package personalizedmealplangenerator;
-
-public class VeganMeal implements MealPlan {
-    @Override
-    public String getMealPlan() {
-        return "Vegan Meal: Includes vegetables, fruits, grains, and plant-based proteins.";
-    }
-}
