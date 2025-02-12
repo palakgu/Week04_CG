1904fcaf967e96d50a66b52a58e5efa593a18dc6
palak gupta <palakofficial21@gmail.com>
2025-02-12 16:15:39 +0530

    [Palak] Refactor. Find Subset in Two set

--- Day02_javaCollections/src/main/java/setinterface/FindSubset.java
+++ Day02_javaCollections/src/main/java/setinterface/FindSubset.java
@@ -5,13 +5,16 @@ import java.util.HashSet;
 
 public class FindSubset {
     public static void main(String[] args) {
-        HashSet<Integer> set1 = new HashSet<>(Arrays.asList(4,6,3));
-        HashSet<Integer> set2 = new HashSet<>(Arrays.asList(4,5,2,6,3));
+        HashSet<Integer> set2 = new HashSet<>(Arrays.asList(4,6,3));
+        HashSet<Integer> set1 = new HashSet<>(Arrays.asList(4,5,2,6,3));
         System.out.println(subset(set1,set2));
     }
     public static boolean subset(HashSet<Integer> set1 , HashSet<Integer>set2){
-        boolean isSubset = set2.containsAll(set1);
-
-        return isSubset;
+        boolean isSubset = set2.containsAll(set1) ;
+         boolean isSubset1 = set1.containsAll(set2);
+         if(isSubset ==true  || isSubset1== true){
+             return true;
+         }
+        return false;
     }
 }
